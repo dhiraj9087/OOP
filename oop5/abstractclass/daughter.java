092457a76c20d0c95ba1761fb0e1310b8a8df006
dhiraj <d7765744@gmail.com>
2024-04-10 17:27:38 +0530

    daughter class

---
+++ oop5/abstractclass/daughter.java
@@ -0,0 +1,21 @@
+public class daughter extends parent{
+
+
+        public daughter(int age) {
+        super(age);
+        //TODO Auto-generated constructor stub
+    }
+
+        @Override
+        void carrer() {
+            System.out.println("i am going to be doc");
+        }
+    
+        @Override
+        void partner() {
+            System.out.println("i love pots");
+        }
+        
+    
+    
+}
